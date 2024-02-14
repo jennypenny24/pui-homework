6e5faf88e863e3c40e99684eab02dc4bf82e8b85
jennypenny24 <157049311+jennypenny24@users.noreply.github.com>
2024-02-14 15:00:05 -0500

    populate options in JS~

--- solution-hw3/random-element.js
+++ solution-hw3/random-element.js
@@ -1,31 +1,41 @@
 const startPrice = 2.49;
 
-const glazePrice = {
-  "original": 0,
-  "sugar": 0,
-  "vanilla": 0.50,
-  "doublechoc": 1.50
+const glazePrices = {
+  "Original": 0,
+  "Sugar": 0,
+  "Vanilla": 0.50,
+  "Double Chocolate": 1.50
 };
 
-const packPrice = {
+const packPrices = {
   "1": 1,
   "3": 3,
   "6": 5,
   "12": 10
 };
+ 
+for (const key in glazePrices) {
+  option = document.createElement("option");
+  option.text = key;
+  option.value = glazePrices[key];
+  document.querySelector("#dropdown1-glazing").appendChild(option);
+}
+
+for (const key in packPrices) {
+  option = document.createElement("option");
+  option.text = key;
+  option.value = packPrices[key];
+  document.querySelector("#dropdown1-pack").appendChild(option);
+}
 
 function calcTotalPrice() {
   selectedGlaze = document.querySelector("#dropdown1-glazing").value;
   selectedPack = document.querySelector("#dropdown1-pack").value;
-  finalPrice = (startPrice + glazePrice[selectedGlaze]) * packPrice[selectedPack];
+  finalPrice = (startPrice + parseFloat(selectedGlaze)) * parseFloat(selectedPack);
   return finalPrice.toFixed(2);
 }
 
 function updatePriceShown() {
   document.querySelector(".detailpricetext").textContent = "$" + calcTotalPrice();
 }
-
-
-
-
 
