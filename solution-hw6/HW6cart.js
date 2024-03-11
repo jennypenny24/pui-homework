8186b4ae565c54c978ddaf5094e8c2b91299272f
jennypenny24 <157049311+jennypenny24@users.noreply.github.com>
2024-03-11 15:11:17 -0400

    hw 6 edits

---
+++ solution-hw6/HW6cart.js
@@ -0,0 +1,147 @@
+const glazePrices = {
+  "Keep original": 0,
+  "Sugar milk": 0,
+  "Vanilla milk": 0.50,
+  "Double Chocolate": 1.50
+};
+
+const packPrices = {
+  "1": 1,
+  "3": 3,
+  "6": 5,
+  "12": 10
+};
+
+let itemsInCart = [];
+
+// Cart page
+function populateCart() {
+  const cartListElement = document.querySelector('.cartmaincontainer');
+  cartListElement.innerHTML = ''; 
+
+  console.log("Items in cart:", itemsInCart); 
+
+  for (const roll of itemsInCart) {
+    createElement(roll);
+  }
+
+  changeTotalPrice(); 
+}
+
+
+// Product detail page
+function addToCart(rollType, rollGlazing, packSize, rollPrice) {
+  const roll = new Roll(rollType, rollGlazing, packSize, rollPrice);
+  itemsInCart.push(roll); // Add the new roll to the cart
+  console.log("Item added to cart:", roll); 
+  saveToLocalStorage(); // Save the updated cart to local storage
+  populateCart(); // Update the cart display
+  updateCartBadge(); // Update the cart badge count
+}
+
+// Roll class
+class Roll {
+  constructor(rollType, rollGlazing, packSize, rollPrice) {
+    this.type = rollType;
+    this.glazing = rollGlazing;
+    this.size = packSize;
+    this.basePrice = rollPrice;
+    this.finalPrice = (this.basePrice + parseFloat(glazePrices[this.glazing])) * parseFloat(packPrices[this.size]);
+    this.element = null;
+  }
+}
+
+// Save cart to local storage
+function saveToLocalStorage() {
+  const cartArrayString = JSON.stringify(itemsInCart);
+  localStorage.setItem('cart', cartArrayString);
+}
+
+// Retrieve cart from local storage
+function retrieveFromLocalStorage() {
+  const cartArrayString = localStorage.getItem('cart');
+  if (cartArrayString) {
+    itemsInCart = JSON.parse(cartArrayString);
+  } else {
+    itemsInCart = [];
+  }
+}
+
+// DOM manipulation functions
+function createElement(roll) {
+  const template = document.querySelector('#cart-template');
+  const clone = template.content.cloneNode(true);
+  roll.element = clone.querySelector('.cartitem1');
+
+  const btnDelete = roll.element.querySelector('.remove');
+  btnDelete.addEventListener('click', () => {
+    const index = itemsInCart.indexOf(roll);
+    if (index !== -1) {
+      removeItem(index);
+    }
+  });
+
+  const cartListElement = document.querySelector('.cartmaincontainer');
+  cartListElement.appendChild(roll.element);
+
+  updateElement(roll);
+}
+
+function updateElement(roll) {
+  const rollImageElement = roll.element.querySelector('.cartimg');
+  const rollPriceElement = roll.element.querySelector('.cartprice');
+  const rollTypeElement = roll.element.querySelector('.type');
+  const rollGlazingElement = roll.element.querySelector('.glazing');
+  const rollSizeElement = roll.element.querySelector('.size');
+
+  finalPrice = (roll.basePrice + parseFloat(glazePrices[roll.glazing])) * parseFloat(packPrices[roll.size]);
+  finalPrice = finalPrice.toFixed(2);
+
+  rollImageElement.src = "assets/products/" + rolls[roll.type]["imageFile"];
+  rollPriceElement.innerText = "$ " + finalPrice;
+  rollTypeElement.innerText = roll.type + " Cinnamon Roll";
+  rollGlazingElement.innerText = roll.glazing;
+  rollSizeElement.innerText = "Pack Size: " + roll.size;
+}
+
+// Initial population of the cart on page load
+retrieveFromLocalStorage();
+populateCart();
+
+// Remove item from cart
+function removeItem(index) {
+  itemsInCart.splice(index, 1);
+  saveToLocalStorage();
+  populateCart();
+  updateCartBadge(); // Update cart badge count
+}
+
+// Calculate and update total price
+function changeTotalPrice() {
+  let totalPrice = 0;
+  for (let i = 0; i < itemsInCart.length; i++) {
+    totalPrice += itemsInCart[i].finalPrice;
+  }
+  const totalPriceElement = document.querySelector('.totalsum');
+  totalPriceElement.innerText = "$ " + totalPrice.toFixed(2);
+}
+
+//CART BADGE ICON PART 
+function updateCartBadge() {
+  const cartBadge = document.querySelector('.cartnumber');
+  cartBadge.innerText = itemsInCart.length;
+}
+
+// Initial population of the cart on page load
+retrieveFromLocalStorage();
+populateCart();
+updateCartBadge(); // Update cart badge count
+
+
+
+
+
+
+
+
+
