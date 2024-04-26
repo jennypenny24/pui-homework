52915b6749e02fa43452cd09446a757c168ab6df
jennypenny24 <157049311+jennypenny24@users.noreply.github.com>
2024-04-25 21:37:55 -0400

    project

---
+++ solution-project/cart.js
@@ -0,0 +1,133 @@
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+      this.type = rollType;
+      this.glazing =  rollGlazing;
+      this.size = packSize;
+      this.basePrice = basePrice;
+  }
+}
+
+// glazing prices object
+const glazingPrices = {
+  "Keep original": 0,
+  "Sugar milk": 0,
+  "Vanilla milk": 0.5,
+  "Double Chocolate": 1.5
+}
+
+// pack size prices object
+const packSizePrices = {
+  "1": 1,
+  "3": 3,
+  "6": 5,
+  "12": 10
+}
+
+function initiateCart() {
+  if (localStorage.getItem('storedRolls') != null){
+      const cart = JSON.parse(localStorage.getItem('storedRolls'));
+      return cart;
+  }
+  else{
+      return [];
+  }
+}
+
+let finalCartPrice = 0;
+
+// cart array
+const cart = initiateCart();
+
+// making a new cart item
+function newCartItem(roll) {
+  const template = document.querySelector('#cart-template');
+  const clone = template.content.cloneNode(true);
+  roll.element = clone.querySelector('.cartitem1');
+
+  const btnRemove = roll.element.querySelector('.remove');
+  btnRemove.addEventListener('click', () => {
+      removeCartItem(roll);
+      updateCartBadge();
+  });  
+
+  const rollListElement = document.querySelector('.cartmaincontainer');
+  rollListElement.prepend(roll.element);
+  updateElement(roll);
+}
+
+// update roll element
+function updateElement(roll) {
+  const rollImageElement = roll.element.querySelector('.cartimg');
+  const rollPriceElement = roll.element.querySelector('.cartprice');
+  const rollTypeElement = roll.element.querySelector('.type');
+  const rollGlazingElement = roll.element.querySelector('.glazing');
+  const rollSizeElement = roll.element.querySelector('.size');
+  const rollCartTotal = document.querySelector('.totalsum')
+  
+  rollTotalPrice = ((roll.basePrice+glazingPrices[roll.glazing])*packSizePrices[roll.size]).toFixed(2);
+  finalCartPrice = finalCartPrice + Number(rollTotalPrice);
+
+  rollImageElement.src = "assets/products/" + rolls[roll.type]["imageFile"];
+  rollPriceElement.innerText = '$' + rollTotalPrice;
+  rollTypeElement.innerText = roll.type + " Cinnamon Roll";
+  rollGlazingElement.innerText = roll.glazing;
+  rollSizeElement.innerText = "Pack Size: " + roll.size;
+  rollCartTotal.innerText = '$' + finalCartPrice;
+}
+
+// retrieve from local storage
+function retrieveFromLocalStorage() {
+  const cartString = localStorage.getItem('storedRolls');
+  const cart = JSON.parse(cartString);
+  for (const rollData of cart){
+      newCartItem(rollData);
+  }
+}
+
+// save to local storage
+function saveToLocalStorage() {
+  const cartString = JSON.stringify(Array.from(cart));
+  console.log(cartString);
+  localStorage.setItem('storedRolls', cartString);
+}
+
+// remove cart item
+function removeCartItem(roll) {
+  roll.element.remove();
+  const {element, ...rest} = Object.assign({}, roll);
+  let index = -1
+  for (let i = 0; i < cart.length; i++) {
+    const cartIndexString = JSON.stringify(cart[i]);
+    if (cartIndexString === JSON.stringify(rest)) {
+      index = i;
+      break;
+    }
+  }
+  cart.splice(index, 1);
+  saveToLocalStorage();
+  rollTotalPrice = ((roll.basePrice + glazingPrices[roll.glazing]) * packSizePrices[roll.size]).toFixed(2);
+  finalCartPrice -= parseFloat(rollTotalPrice); 
+  if (cart.length === 0) {
+    finalCartPrice = 0;
+  }
+  document.querySelector('.totalsum').innerText = '$' + finalCartPrice.toFixed(2);
+}
+
+
+// call retrieveFromLocalStorage
+if (localStorage.getItem('storedRolls') != null){
+  retrieveFromLocalStorage();
+}
+updateCartBadge();
+
+// change cart badge
+function updateCartBadge() {
+  const cartCountElement = document.getElementById('cart-count');
+  cartCountElement.innerText = cart.length;
+}
+
+
+
+
+
+
---
+++ solution-project/script.js
@@ -0,0 +1,161 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+
+
+/*card flip
+var thecard = document.querySelectorAll('.thecard');
+thecard.addEventListener("click", flipCard);
+
+function flipCard() {
+    thecard.classList.toggle("flipCard");
+}
+*/
+
+/*card flip version 2*/
+
+var cardToFlip = document.querySelectorAll('.thecard');
+
+    for (var thecard of cardToFlip){
+    thecard.addEventListener("click", toggleToFlip);
+    }
+
+function toggleToFlip() {
+    this.classList.toggle("flipCard");
+}
+
+
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
+
+// Initially hide the text
+$('.text span').css('opacity', 0);
+
+// Loop through each letter
+$('.text span').each(function(index) {
+    // Delay the animation for each letter
+    $(this).delay(200 * index).animate({opacity: 1}, 500); // Adjust timing as needed
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the <a> elements inside the navigation bar
+  const navLinks = document.querySelectorAll('nav a');
+
+  // Loop through each <a> element
+  navLinks.forEach(function(navLink) {
+    // Add click event listener to each <a> element
+    navLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+
+      // Get the target section's ID from the href attribute of the clicked link
+      const targetId = navLink.getAttribute('href');
+
+      // Use the smooth scroll function to scroll to the target section
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('.toc a');
+
+  links.forEach(link => {
+      link.addEventListener('click', scrollToSection);
+  });
+
+  function scrollToSection(e) {
+      e.preventDefault();
+      const targetId = e.target.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      window.scrollTo({
+          top: targetSection.offsetTop - 20,
+          behavior: 'smooth'
+      });
+  }
+});
+
+
+
+
+
+
+
